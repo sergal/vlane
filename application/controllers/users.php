2699f6057cbd9ad0cee9780a2c0704b51cc4788b
Admi <shelko@local.host>
2012-08-21 13:04:29 +0700

    User's controller

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -40,7 +40,7 @@ class Users extends CI_Controller
         $this->load->view("header");
         $this->load->model("User_model");
         $data["users"] = $this->User_model->get_by_group($group);
-        $data["group_name"] = $this->Group_model->get_group($group);
+        $data["group"] = $this->Group_model->get_group($group);
         $this->load->view("users/group", $data);
         $this->load->view("footer");
     }
