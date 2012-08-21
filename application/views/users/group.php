a1f24644dbf86382be60ab43fa8bc295eea7453a
Dima <Sokolov@local.host>
2012-08-21 15:03:55 +0700

    add show group page

--- application/views/users/group.php
+++ application/views/users/group.php
@@ -4,16 +4,15 @@
             <center>Отряд
         </strong></center></h1>
     </div>
-    <div class="span12">
+    <div class="span6">
         <ul>
             <?php foreach ($users as $user) : ?>
             <li>
                 <?php
-                echo $user["name"];
+                echo" <a href=/users/show/".$user["id"].">".$user["name"]."</a>";
 ?>
             </li>
             <?php endforeach ?>
         </ul>
-
     </div>
 </div>
