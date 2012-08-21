7b295d71f063b5e983db46514e7ed853b19d8e6a
Andrey <latyshev@local.host>
2012-08-21 11:23:07 +0700

    crEATED two new models: get_by_school, get_by_city

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -12,6 +12,17 @@ class User_model extends CI_Model
         $user = $this->db->get_where('users', array('id' => $id));
         return $user->row_array();
     }
+
+    public function get_by_school($name_school)
+    {
+        $user = $this->db->get_where('user', array('school' => $name_school));
+        return $user->result_array();
+    }
+
+    public function get_by_city($city){
+        $user = $this->get_where('user', array('city' => $city));
+        return $user->result_array();
+    }
 }
 
 
