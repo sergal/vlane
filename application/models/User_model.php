d2bf784c22c449b201d84802d4d6ca7e3460f807
Admi <zhuravleva@local.host>
2012-08-21 11:58:08 +0700

    add model for user

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -4,13 +4,25 @@ class User_model extends CI_Model
     public function __construct()
     {
         $this->load->database();
-        parent__construct();
+        parent::__construct();
     }
 
-    public function get_user($id)
+    public function get_users($id)
     {
         $user = $this->db->get_where('users', array('id' => $id));
         return $user->row_array();
+    }
+
+    public function get_by_group($group_id)
+    {
+        $this->db->select("user_id");
+        $arr_users = $this->db->get_where('members', array('group_id' => $group_id));
+        foreach ($arr_users as $elem) {
+            $users[] = $this->get_user($elem["user_id"]);
+        }
+        return $users;
+
+
     }
 }
 
