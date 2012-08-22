82c82d23078e3f8199e3f6f1a00995007bf55fed
Dima <lisunov@local.host>
2012-08-22 10:48:44 +0700

    change login page

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -2,5 +2,7 @@
 $this->load->helper('index');
 echo form_open('users/login');
 echo form_input('login');
+echo "<br>";
 echo form_password('password');
+echo "<br>";
 echo form_submit('submit','Войти');
