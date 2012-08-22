fb45f136f1539cc36da16ecc4451c2992d267619
Admi <shelko@local.host>
2012-08-22 11:20:35 +0700

    Controllers

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,3 +1,4 @@
+<meta charset="UTF-8">
 <?php
 /*
  * Контроллер информации о пользователе
@@ -64,11 +65,19 @@ class Users extends CI_Controller
             );
             $this->session->set_userdata($user_data);
             $test = $this->session->userdata('username');
-            echo $test; die;
             redirect('/users/show/' . $result['id'], 'location');
         } else {
             $this->load->view('users/login');
         }
         $this->load->view("footer");
+    }
+    public function logout(){
+        $this->load->helper('url');
+        $this->load->helper('cookie');
+        $this->load->library('session');
+       if($this->input->cookie('ci_session')!=null){
+           $this->session->sess_destroy();
+           $this->load->view('users/login');
+       }
     }
 }
