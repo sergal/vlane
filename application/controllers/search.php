28d2393203a3b7f155c7bd4b505b485f6d3856f2
maximmi <metel@localhost>
2012-08-22 09:04:26 +0400

    Поиск работает

--- application/controllers/search.php
+++ application/controllers/search.php
@@ -13,6 +13,12 @@ public function process()
 {
     $this->load->model("Search_model");
     $data["result"] = $this->Search_model->search_name($this->input->post("txt"));
+    $i = 0; //можете лучше - перепишите!
+    foreach ($data["result"] as $num)
+    {
+        $data["group"][$i] = $this->Search_model->search_group($num["id"]);
+        $i++;
+    }
     $this->load->view("header");
     $this->load->view("users/search_result", $data);
     $this->load->view("footer.php");
--- application/views/users/search_result.php
+++ application/views/users/search_result.php
@@ -1,6 +1,7 @@
 <table class="table table-bordered table-striped">
-<thead><tr><td>Имя</td><td>Город</td><td>Школа</td><td>Класс</td></tr></thead><tbody>
+<thead><tr><td>Имя</td><td>Город</td><td>Школа</td><td>Класс</td><td>Отряд</td></tr></thead><tbody>
 <?php
+$i = 0;
 foreach ($result as $num)
 {
 echo "<tr><td>";
@@ -11,6 +12,9 @@ echo "</td><td>";
 echo $num["school"];
 echo "</td><td>";
 echo $num["class"];
+echo "</td><td>";
+echo $group[$i][0]["name"]." aka ".$group[$i][0]["nickname"];
 echo "</td></tr>";
+$i++;
 }
 ?></tbody></table>
