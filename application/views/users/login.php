b3c113a1045a0a08c0a9abbf5c1ed4627736cd1c
Dima <lisunov@local.host>
2012-08-22 10:38:36 +0700

    changed form in login.php

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -1,6 +1,8 @@
 <?php
-$this->load->helper('index');
+$this->load->helper('form');
 echo form_open('users/login');
 echo form_input('login');
+echo "<br>";
 echo form_password('password');
+echo "<br>";
 echo form_submit('submit','Войти');
