cccae8489f6794b326946f90386ed3054b952948
maximmi <metel@localhost>
2012-08-22 18:38:46 +0400

    Friends delete function added

--- application/views/header.php
+++ application/views/header.php
@@ -1,10 +1,13 @@
+<?php $this->load->helper('url');
+$this->load->library('session');
+$uid = $this->session->userdata('user_id'); ?>
 <!DOCTYPE html>
 <html>
 <head>
     <META CHARSET="UTF-8">
     <title>вЛАНе</title>
-    <link href="http://localhost/web/css/bootstrap.css" rel="stylesheet">
-    <link rel="icon" href="http://localhost/application/views/favicon.ico" type="image/x-icon"/>
+    <link href="<?php echo base_url("web/css/bootstrap.css"); ?>" rel="stylesheet">
+    <link rel="icon" href="<?php echo base_url("web/favicon.ico"); ?>" type="image/x-icon"/>
 </head>
 <body>
 <div class="navbar">
@@ -21,16 +24,16 @@
             <div class="nav-collapse">
                 <ul class="nav">
                     <?php if ($user_id != null) : ?>
-                    <li <?php if ($active_page == 0) { echo "class=active"; }?>><a href="http://localhost/index.php/users/show">Моя страница</a></li>
+                    <li <?php if ($active_page == 0) { echo "class=active"; }?>><a href="<?php echo site_url("users/show/".$uid); ?>">Моя страница</a></li>
                     <?php endif ?>
-                    <li <?php if ($active_page == 1) { echo "class=active"; }?>><a href="http://localhost/index.php/groups/show">Отряды</a></li>
+                    <li <?php if ($active_page == 1) { echo "class=active"; }?>><a href="<?php echo site_url("groups/show"); ?>">Отряды</a></li>
                     <?php if ($user_id != null) : ?>
                     <li <?php if ($active_page == 2) { echo "class=active"; }?>><a
-                        href="http://localhost/index.php/messages/show/<?php echo $user_id; ?>">Сообщения<?php if ($messages > 0) {
+                        href="<?php echo site_url("messages/show/".$user_id); ?>">Сообщения<?php if ($messages > 0) {
                         echo ' (' . $messages . ')';
                     }
                         ?>
-                        <li <?php if ($active_page == 3) { echo "class=active"; }?>><a href="http://localhost/index.php/users/get_friends">Друзья</a></li>
+                        <li <?php if ($active_page == 3) { echo "class=active"; }?>><a href="<?php echo site_url("users/get_friends"); ?>">Друзья</a></li>
                     </a></li><?php endif ?>
                 </ul>
                 <form class="navbar-search pull-left" method="post" action="http://localhost/index.php/search/process">
@@ -39,12 +42,12 @@
                 <?php if ($user_id != null) : ?>
 
                 <ul class="nav pull-right">
-                    <li><a href="http://localhost/index.php/users/logout">Выйти</a></li>
+                    <li><a href="<?php echo site_url("users/logout"); ?>">Выйти</a></li>
 
                 </ul>
                 <?php else : ?>
                 <ul class="nav pull-right">
-                    <li><a href="http://localhost/index.php/users/login">Войти</a></li>
+                    <li><a href="<?php echo site_url("users/login"); ?>">Войти</a></li>
 
                 </ul>
                 <?php endif ?>
