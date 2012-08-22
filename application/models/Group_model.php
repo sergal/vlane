fb6d3e82c1d985e34bef8497cc5adec83e9c395b
Andrey <latyshev@local.host>
2012-08-22 11:04:12 +0700

    fixed get_by_user 1.0

--- application/models/Group_model.php
+++ application/models/Group_model.php
@@ -15,7 +15,7 @@ class Group_model extends CI_Model
 
     public function get_by_user($user_id)
     {
-        $this->db->select('name, nickname, year, type');
+        $this->db->select('name, nickname, year, type, groups.id');
         $this->db->from('members');
         $this->db->join('groups', 'groups.id = members.group_id');
         $this->db->join('seasons', 'groups.season_id = seasons.id');
