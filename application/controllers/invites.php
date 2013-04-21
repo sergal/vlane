374ff376930001625475839ac0f2c88e5e3dd24e
Kostya Shelko <kostynru@ymail.com>
2013-04-21 20:43:00 +0700

    finished invites system

--- application/controllers/invites.php
+++ application/controllers/invites.php
@@ -1,20 +1,27 @@
 <?php
 class Invites extends Base_Controller {
-    public function compare_tokens($id, $token){
-        $this->load('Invites_model');
-        $this->load('User_model');
+    public function activate($id, $token){
+        $this->set_header();
+        $this->load->model('Invites_model');
+        $this->load->model('User_model');
         $token_r = $this->Invites_model->get_token_by_id($id);
         if(!$token_r){
             $data['token_status'] = FALSE;
-        }
+        } else {
         if($token == $token_r){
             $res = $this->_rand_login_and_password();
             $login = $res['login'];
             $password = $res['pass'];
             $this->User_model->register_user($login, $password);
             $this->Invites_model->token_used($token);
+            $data['login'] = $login;
+            $data['password'] = $password;
+            $data['token_status'] = TRUE;
+            $this->load->view('invites/activate', $data);
+            $this->load->view('footer');
         } else {
             throw new Exception('Something went wrong');
+        }
         }
     }
     private function _rand_login_and_password(){
@@ -24,12 +31,12 @@ class Invites extends Base_Controller {
             $rand = rand(0,1);
             switch($rand){
                 case 0:
-                    $login .= rand(1, 9);
-                    $pass .= rand(1, 9);
+                    $login .= rand(0, 9);
+                    $pass .= rand(0, 9);
                 break;
                 case 1:
-                    $login .= chr(rand(0, 127));
-                    $pass .= chr(rand(0, 127));
+                    $login .= chr(rand(97, 122));
+                    $pass .= chr(rand(65, 122));
                 break;
             }
         }
